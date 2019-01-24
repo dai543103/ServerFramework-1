7f5a9724b3f7158658379560854407bab7394b4c
A1meng <784108273@qq.com>
2019-01-24 16:42:11 +0800

    暂时取消安全性检查

--- 101_ZoneServer/Login/LoginHandler.cpp
+++ 101_ZoneServer/Login/LoginHandler.cpp
@@ -70,6 +70,7 @@ int CLoginHandler::NotifyRoleLogin(CGameRoleObj* pstRoleObj)
 
 int CLoginHandler::SecurityCheck(const TNetHead_V2& rstNetHead)
 {
+	// todo ameng ÔİÊ±²»¼ì²é
     unsigned int uiUin = m_pRequestMsg->sthead().uin();
     if (uiUin == 0)
     {
